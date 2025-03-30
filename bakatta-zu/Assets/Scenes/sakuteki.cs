8e9f60494ae706e8916eef414e55d302544e31d7
wonderEx24 <wd.shibuya.op@gmail.com>
2025-03-30 11:27:58 +0900

    たなかかくえい

--- bakatta-zu/Assets/Scenes/sakuteki.cs
+++ bakatta-zu/Assets/Scenes/sakuteki.cs
@@ -1,19 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class sakuteki : MonoBehaviour
 {
+    
     public Transform player;
     // Start is called before the first frame update
-    void Start()
+    async Task Start()
     {
         
-    }(
-    void OnTriggerStay(Collider col){
-        if(col.gameObject.name == "player"){
+    }
+    void OnTriggerStay(Collider col)
+    {
+        if(col.gameObject.name == "player")
+        {
             transform.LookAt(player);
-            transform.Translate(0,0,0.1f)
+            transform.Translate(0,0,0.1f);
+             Debug.Log("みつけた");
         }
     }
 
@@ -23,8 +28,4 @@ public class sakuteki : MonoBehaviour
         
     }
 
-    void OnTriggerStay(Collider col)
-    {
-        Debug.Log("みつけた");
-    }
 }
