9128720bb00fc38c8f382ff9cd0de4004ab0f30d
wonderEx24 <wd.shibuya.op@gmail.com>
2025-09-07 11:10:31 +0900

    今日は帰国して初めてのリタリコです。今までやってきたことがまったくわからなくなり、現在ほとんど詰みの状態。進捗管理表も機能していないと言っても過言ではなく、やるべきことを手探りで探しているところであります。

--- bakatta-zu/Assets/Plugins/DC Code/Scenes/c#/ImpressionManager.cs
+++ bakatta-zu/Assets/Plugins/DC Code/Scenes/c#/ImpressionManager.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ImpressionManager : MonoBehaviour
-{
+{/*
     public int npcCount = 30;
     public float initialReachChance = 0.5f;
     public float spreadChance = 0.3f;
     public int spreadDepth = 3;
 
-    public Text impressionText;
+    //public Text impressionText;
 
 
-    private List<NPC> npcs = new List<NPC>();
-    private HashSet<NPC> seenNPCs = HashSet<NPC>();
+    //private List<NPC> npcs = new List<NPC>();
+    //private HashSet<NPC> seenNPCs = HashSet<NPC>();
     // Start is called before the first frame update
     void Start()
     {
@@ -37,12 +37,13 @@ public class ImpressionManager : MonoBehaviour
     int realCount = seenNPCs.Count;
     int scaledCount = Mathf.RoundToInt(realCount * scaleFactor);
 
-    impressionText.text = $"インプレッション数: {scaledCount:NO}";
-    Debug.Log
-
+    //impressionText.text = $"インプレッション数: {scaledCount:NO}";
+    //Debug.Log("");
+ß
     // Update is called once per frame
     void Update()
     {
         
     }
+    */
 }
